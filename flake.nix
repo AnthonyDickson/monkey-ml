5c3ac7affa7fd5e9f3e35926c9dcb1e9cac50de5
Anthony Dickson <anthony.dickson9656@gmail.com>
2025-08-18 20:19:54 +1200

    Add nix.flake for basic packages following https://ocaml.org/docs/installing-ocaml

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "OCaml Dev Environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default =
+          with pkgs;
+          mkShell {
+            packages = with pkgs; [
+              ocaml
+              ocamlPackages.utop
+              ocamlPackages.dune_3
+              ocamlPackages.odoc
+              ocamlPackages.ocaml-lsp
+              ocamlPackages.ocamlformat
+              ocamlPackages.alcotest
+            ];
+          };
+      }
+    );
+}
