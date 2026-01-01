1d725f9bbd2b02072572c10640b04ea84888adcd
Anthony Dickson <anthony.dickson9656@gmail.com>
2026-01-02 09:51:19 +1300

    chore: remove dead code

--- test/test_ast.ml
+++
@@ -1,7 +0,0 @@
-open Monkeylang
-
-let test_parses_let_statement () =
-  let input = [Token.Let, ]
-  Alcotest.(check bool) "foo" true true
-
-let test_suite = [ Alcotest.test_case "let statements" `Quick test_parses_let_statement ]
